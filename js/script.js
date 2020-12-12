eaa324a27d4d183c080eb3005e0b2c18f3a2c16a
jibiking <daiki.jibiki@gmail.com>
2020-12-13 01:11:29 +0900

    slider完成

--- js/script.js
+++ js/script.js
@@ -1,52 +1,28 @@
 $(function(){
 
   // slide-menu
-  var dir = -1;
-  var interval = 3000;
-  var duration = 700;
-  var timer;
-  var count = $(".fv__slide li").length;
-  
-  $(".fv__slide").prepend($(".fv__slide li:last-child"));
-  $(".fv__slide").css("left", -720);
-  timer = setInterval(slideTimer, interval);
-
-  function slideTimer(){
-    if(dir == -1){
-
-      $(".fv__slide").animate({"left" : "-=960px"},duration,
-      function(){
-        $(this).append($(".fv__slide li:first-child"));
-        $(this).css("left", -720);
-      });
-    }else{
-      $(".fv__slide").animate({"left" : "+=960px"},duration,
-      function(){
-        $(this).prepend($(".fv__slide li:last-child"));
-        $(this).css("left", -720);
-        dir = -1;
-      });
-    }
-  }
-
-  $(".arrow_l").click(function(){
-    dir = 1;
-    clearInterval(timer);
-    timer = setInterval(slideTimer, interval);
-    slideTimer();
-    return false;
-  });
-
-  $(".arrow_r").click(function(){
-    dir = -1;
-    clearInterval(timer);
-    timer = setInterval(slideTimer, interval);
-    slideTimer();
-    return false;
-  });
+  // slick
+  $('.fv__slide').slick({
+    infinite: true,
+    arrows: true,
+    prevArrow:'<button class="arrow_l"></button>',
+    nextArrow:'<button class="arrow_r"></button>',
+    slidesToShow: 1,
+    dots:true,
+    centerMode: true, //要素を中央寄せ
+    centerPadding:'240px', //両サイドの見えている部分のサイズ
+    autoplay:true, //自動再生
+    responsive: [{
+         breakpoint: 480,
+              settings: {
+                   centerMode: false,
+         }
+    }]
+});
+  // //slide-menu
 
 
-  // //slide-menu未完成
+
 
 
   // shop
